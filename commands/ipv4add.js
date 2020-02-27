17e34a37a8b8f1d618fbd1f134c67f3158e901cc
serif54 <61537546+serif54@users.noreply.github.com>
2020-02-27 10:27:58 +0200

    Update ipv4add.js

--- commands/ipv4add.js
+++ commands/ipv4add.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ipv4',
     description: 'ipv4',
     execute(message, args) {
-        message.channel.send('ipv4 is the common format for writing the server IP address. It consists of a 32-bit format of 10 numbers in a XX.XXX.XXX.XX layout')
+        message.channel.send('IPv4 is the common format for writing the server IP address. It consists of a 32-bit format of 10 numbers in a XX.XXX.XXX.XX layout')
     },
 };
